11aa82e42adeaaba94a480eeb630a0805a022554
Scott McCormack <scott.mccormack@articulate.com>
2016-10-11 22:56:32 -0400

    Handle multiple tags per sign

--- client/ducks/signs.js
+++ client/ducks/signs.js
@@ -12,13 +12,13 @@ const IO = require('../lib/io')
 
 const ns = concat('sp/signs/')
 
-const RESIZE       = ns('RESIZE')
-const SET_CATEGORY = ns('SET_CATEGORY')
-const SHOW_MORE    = ns('SHOW_MORE')
+const RESIZE    = ns('RESIZE')
+const SET_TAG   = ns('SET_TAG')
+const SHOW_MORE = ns('SHOW_MORE')
 
 const init = {
   cards,
-  category: 'all',
+  tag: 'all',
   page: last(pages),
   pages: 1
 }
@@ -27,13 +27,13 @@ const page = width =>
   pages.find(config => config.width <= width)
 
 const reducer = p.handle(init, {
-  [ RESIZE       ]: (state, width) => assoc('page', page(width), state),
-  [ SET_CATEGORY ]: flip(assoc('category')),
-  [ SHOW_MORE    ]: over(lensProp('pages'), inc),
+  [ RESIZE    ]: (state, width) => assoc('page', page(width), state),
+  [ SET_TAG   ]: flip(assoc('tag')),
+  [ SHOW_MORE ]: over(lensProp('pages'), inc),
 })
 
-reducer.resize      = elm => p.action(RESIZE, IO(_ => elm.clientWidth))
-reducer.setCategory = p.action(SET_CATEGORY)
-reducer.showMore    = p.action(SHOW_MORE)
+reducer.resize   = elm => p.action(RESIZE, IO(_ => elm.clientWidth))
+reducer.setTag   = p.action(SET_TAG)
+reducer.showMore = p.action(SHOW_MORE)
 
 module.exports = reducer
--- client/views/signs.js
+++ client/views/signs.js
@@ -1,43 +1,45 @@
-const append  = require('ramda/src/append')
-const compose = require('ramda/src/compose')
-const filter  = require('ramda/src/filter')
-const I       = require('ramda/src/identity')
-const p       = require('puddles')
-const pluck   = require('ramda/src/pluck')
-const propEq  = require('ramda/src/propEq')
-const sortBy  = require('ramda/src/sortBy')
-const T       = require('ramda/src/T')
-const take    = require('ramda/src/take')
-const uniq    = require('ramda/src/uniq')
+const append   = require('ramda/src/append')
+const compose  = require('ramda/src/compose')
+const contains = require('ramda/src/contains')
+const filter   = require('ramda/src/filter')
+const flatten  = require('ramda/src/flatten')
+const I        = require('ramda/src/identity')
+const p        = require('puddles')
+const pluck    = require('ramda/src/pluck')
+const prop     = require('ramda/src/prop')
+const sortBy   = require('ramda/src/sortBy')
+const T        = require('ramda/src/T')
+const take     = require('ramda/src/take')
+const uniq     = require('ramda/src/uniq')
 
 const Card      = require('./card')
 const Footer    = require('./footer')
 const Layout    = require('./layout')
 const { paged } = require('../lib/animations')
-const { setCategory, showMore } = require('../ducks/signs')
+const { setTag, showMore } = require('../ducks/signs')
 
-const byCategory = name =>
-  name === 'all' ? T : propEq('category', name)
+const byTag = name =>
+  name === 'all' ? T : compose(contains(name), prop('tags'))
 
-const categories = compose(sortBy(I), append('all'), uniq, pluck('category'))
+const tags = compose(sortBy(I), append('all'), uniq, flatten, pluck('tags'))
 
 const Signs = ({ signs }) => {
-  const { cards, category, page, pages } = signs,
+  const { cards, tag, page, pages } = signs,
         total = pages * page.size
 
-  const select = filter(byCategory(category)),
+  const select = filter(byTag(tag)),
         limit  = compose(take(total), select)
 
   return p('div.signs.page', { style: paged }, [
-    p('nav.categories.row', categories(cards).map(name =>
-      p('div.category', {
-        class: { active: category === name },
-        on: { click: [ setCategory, name ] }
+    p('nav.tags.row', tags(cards).map(name =>
+      p('div.tag', {
+        class: { active: tag === name },
+        on: { click: [ setTag, name ] }
       }, name)
     )),
 
     p('div.content', [
-      p('div.page', { key: category, style: paged }, [
+      p('div.page', { key: tag, style: paged }, [
         p('div.cards.row', limit(cards).map(Card)),
 
         p('button.more', {
